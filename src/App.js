291a02f2787ad4e1eb734a12f4d78ea0d37da0f4
Ang Sherpa <ang.sherpa@sinnerschrader.com>
2022-07-05 10:46:23 +0200

    edited the App.js to trigger the github actions

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a dummy app to test the Lighthouse in Github actions.
         </p>
         <a
           className="App-link"
