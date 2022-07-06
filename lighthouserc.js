1431194316877812785b68c459719fdb2ced0bd7
Ang Sherpa <ang.sherpa@sinnerschrader.com>
2022-07-06 15:24:45 +0200

    access the wizard build token as environment variable

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,12 +1,12 @@
 module.exports = {
   ci: {
     collect: {
-      numberOfRuns: 4,
+      numberOfRuns: 3,
     },
     upload: {
       target: 'lhci',
       serverBaseUrl: 'https://lighthousearchive.herokuapp.com/',
-      token: '30038a30-2d6e-4239-b881-34de94746a16'
+      token: process.env.LHCI_WIZARD_BUILD_TOKEN
     }
   },
 };
