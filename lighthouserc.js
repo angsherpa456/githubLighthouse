3f70cddba1e7620e515130d5f57c787cad7f73d9
angsherpa456 <89925822+angsherpa456@users.noreply.github.com>
2022-07-14 12:53:12 +0200

    assertion implemented for warning. Budget.json file removed (#13)
    Co-authored-by: Ang Sherpa <ang.sherpa@sinnerschrader.com>

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,10 +3,8 @@ const puppeteer = require('puppeteer');
 module.exports = {
   ci: {
     collect: {
-      // url: ['https://www.flaconi.de/', 'https://www.flaconi.de/parfum/', 'https://www.flaconi.de/parfum/chanel/bleu-de-chanel/chanel-bleu-de-chanel-eau-de-parfum.html#sku=80004552-50'],
-      // url: ['https://lite-stage-de.flaconi.de/', 'https://lite-stage-de.flaconi.de/pflege/', 'https://lite-stage-de.flaconi.de/pflege/m2-beaute/eyelash/m2-beaute-eyelash-activating-serum-wimpernserum.html#sku=80045755-5'],
-      url: ['https://lite-stage-de.flaconi.de/'], // TODO: use above three staging urls
-      numberOfRuns: 1, // TODO: make this 3 in the final
+      url: ['https://lite-stage-de.flaconi.de/', 'https://lite-stage-de.flaconi.de/pflege/', 'https://lite-stage-de.flaconi.de/pflege/m2-beaute/eyelash/m2-beaute-eyelash-activating-serum-wimpernserum.html#sku=80045755-5'],
+      numberOfRuns: 3,
       puppeteerScript: 'login-puppeteer-script.js',
       chromePath: puppeteer.executablePath(),
       puppeteerLaunchOptions: {
@@ -15,13 +13,13 @@ module.exports = {
       }
     },
     assert: {
-      budgetsFile: "./budget.json"
+      assertions: {
+        "categories:performance": ["warn", {"minScore": 0.9}],
+        "categories:accessibility": ["warn", {"minScore": 1}]
+      }
     },
     upload: {
       target: 'temporary-public-storage'
-      // target: 'lhci',
-      // serverBaseUrl: 'https://lighthousearchive.herokuapp.com/',
-      // token: process.env.LHCI_WIZARD_BUILD_TOKEN
     },
   },
 };
